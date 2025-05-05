bf9870d7c0f6136aa5704b9dee1b066efb4eb12f
Daniel Guo <dguo8412@gmail.com>
2025-05-05 16:19:04 -0700

    remove depricated init

--- language/__init__.py
+++ language/__init__.py
@@ -3,4 +3,3 @@ from .base import ProgramSequence, ProgramGenerator, ProgramConstraint
 from .sequence import ProgramDNASequence, ProgramRNASequence, ProgramProteinSequence
 from .generator import UniformMutationGenerator, Evo2Generator, SemanticMiningGenerator, BindCraftGenerator
 from .program import Program
-from .utils import load_fasta_sequences_phix
