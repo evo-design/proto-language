25f454689dd2e33a97ee47b55e22d4591d1a267c
Brian Hie <blhie1@gmail.com>
2026-05-13 09:56:06 -0700

    feat: expose compiled gradient support metadata

--- proto_language/language/optimizer/constraint_compiler/__init__.py
+++ proto_language/language/optimizer/constraint_compiler/__init__.py
@@ -4,6 +4,11 @@ from proto_language.language.optimizer.constraint_compiler.base import (
     GradientProvider,
     GradientProviderOutput,
 )
+from proto_language.language.optimizer.constraint_compiler.capabilities import (
+    GradientSupport,
+    GradientSupportRule,
+    gradient_support_for_constraint_spec,
+)
 from proto_language.language.optimizer.constraint_compiler.compiler import (
     DirectGradientProvider,
     compile_gradient_providers,
@@ -13,9 +18,12 @@ from proto_language.language.optimizer.constraint_compiler.compiler import (
 
 __all__ = [
     "DirectGradientProvider",
+    "GradientSupport",
+    "GradientSupportRule",
     "GradientProvider",
     "GradientProviderOutput",
     "compile_gradient_providers",
     "constraint_supports_compiled_gradient",
     "evaluate_scoring_constraints",
+    "gradient_support_for_constraint_spec",
 ]
